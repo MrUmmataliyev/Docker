25e2550e6876b429e86782acc47813b38ca77084
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-05-09 09:35:21 +0500

    say

--- src/Courses/Courses.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Courses.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/Courses/Courses.Domain/Models/CourseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Domain.Models
+{
+    public class CourseModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset StartedDate { get; set; }
+        public DateTimeOffset FinishDate { get; set; }
+    }
+}
